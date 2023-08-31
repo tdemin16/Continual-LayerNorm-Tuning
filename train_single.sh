4151123bf6980adf72fe00c80c5576bc6c6386d4
Thomas De Min <thomasdemin16@gmail.com>
2023-08-31 10:28:45 +0200

    Update train_single.sh

--- train_single.sh
+++ train_single.sh
@@ -6,10 +6,10 @@ conda activate cln
 
 # Train in single stage using classifier rectification, alpha and shuffling the order of classes
 
+SEED=0
 TORCH_DISTRIBUTED_DEBUG=DETAIL \
 CUDA_LAUNCH_BLOCKING=1 \
 CUDA_VISIBLE_DEVICES=0 \
-SEED=0
 torchrun --nproc_per_node=1 \
          --master_port=29407 \
         main.py imr \
